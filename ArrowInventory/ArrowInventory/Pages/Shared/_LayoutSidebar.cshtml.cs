7817f76295e24a09d2313cca60b73e7cc761a829
Daniel Fletcher <Daniel.Fletcher@arrowlabs.wales>
2026-06-03 22:38:11 +0100

    - Added - Created a sidebar for Add/admin/manage pages - Updatred site.css for the new navbar - Modified home page navbar to be transparent

---
+++ ArrowInventory/ArrowInventory/Pages/Shared/_LayoutSidebar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ArrowInventory.Pages.Shared
+{
+    public class _LayoutSidebarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
